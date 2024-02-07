5224c77adf840054ba35e365b74da2c8402e72ad
CosmeNicolas <nicomas7590@gmail.com>
2024-02-07 16:53:10 -0300

    le di formato al codigo frmulario pelicula

--- src/components/FormularioPelicula.jsx
+++ src/components/FormularioPelicula.jsx
@@ -1,120 +1,123 @@
-import { useForm } from "react-hook-form"
-import { Button, Container, Form } from 'react-bootstrap';
+import { useForm } from "react-hook-form";
+import { Button, Container, Form } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import Peliculas from "./Peliculas";
-import Swal from 'sweetalert2';
-
-
+import Swal from "sweetalert2";
 
 const FormularioPelicula = () => {
-  const { register, handleSubmit, formState: { errors }, reset } = useForm();
-  const peliculasGuardadas = JSON.parse(localStorage.getItem('guardarPelicula')) || [];
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+  const peliculasGuardadas =
+    JSON.parse(localStorage.getItem("guardarPelicula")) || [];
   const [peliculas, setPeliculas] = useState(peliculasGuardadas);
 
-  const borrarPelicula = (nombrePelicula)=>{
-    const tomarPelicula = peliculas.filter((pelicula)=> pelicula !== nombrePelicula);
-    setPeliculas(tomarPelicula)
-  }
+  const borrarPelicula = (nombrePelicula) => {
+    const tomarPelicula = peliculas.filter(
+      (pelicula) => pelicula !== nombrePelicula
+    );
+    setPeliculas(tomarPelicula);
+  };
 
   useEffect(() => {
-    localStorage.setItem('guardarPelicula',JSON.stringify(peliculas))
-  }, [peliculas])
-  
+    localStorage.setItem("guardarPelicula", JSON.stringify(peliculas));
+  }, [peliculas]);
 
   const onSubmit = (data) => {
-
     const nuevaPelicula = {
       nombrePelicula: data["Nombre Pelicula"],
       descripcion: data["Descripcion"],
       genero: data["genero"],
-      rutaImg: data["rutaImg"]
+      rutaImg: data["rutaImg"],
     };
-    console.log(nuevaPelicula)
-    console.log(errors)
+    console.log(nuevaPelicula);
+    console.log(errors);
 
-    setPeliculas([...peliculas, nuevaPelicula])
+    setPeliculas([...peliculas, nuevaPelicula]);
     reset();
 
     Swal.fire({
-      title: 'Película Agregada',
-      text: 'La película se ha agregado exitosamente.',
-      icon: 'success',
-      confirmButtonColor: '#116A7B'
+      title: "Película Agregada",
+      text: "La película se ha agregado exitosamente.",
+      icon: "success",
+      confirmButtonColor: "#116A7B",
     });
   };
 
-    return (
-      <Container >
-        <Form className="p-3" onSubmit={handleSubmit(onSubmit)}>
-          {/* Nombre Pelicula */}
-          <Form.Group className="mb-3" >
-            <Form.Label className="text-light">Nombre de Pelicula</Form.Label>
-            <Form.Control type="text"
-              placeholder="Nombre Pelicula"
-              {...register("Nombre Pelicula",
-                {
-                  required: true,
-                  min: 3,
-                  maxLength: 50
-                })} />
+  return (
+    <Container>
+      <Form className="p-3" onSubmit={handleSubmit(onSubmit)}>
+        {/* Nombre Pelicula */}
+        <Form.Group className="mb-3">
+          <Form.Label className="text-light">Nombre de Pelicula</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Nombre Pelicula"
+            {...register("Nombre Pelicula", {
+              required: true,
+              min: 3,
+              maxLength: 50,
+            })}
+          />
+        </Form.Group>
+        {/* Descripcion */}
+        <Form.Group className="my-3">
+          <Form.Label className="text-light">Descripción</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={4}
+            type="text"
+            placeholder="Descripción"
+            {...register("Descripcion", {
+              required: true,
+              min: 3,
+              maxLength: 200,
+            })}
+          />
+        </Form.Group>
 
-          </Form.Group>
-          {/* Descripcion */}
-          <Form.Group className="my-3">
-            <Form.Label className="text-light">Descripción</Form.Label>
-            <Form.Control
-              as="textarea"
-              rows={4}
-              type="text" placeholder="Descripción"
-              {...register("Descripcion",
-                {
-                  required: true,
-                  min: 3,
-                  maxLength: 200
-                })} />
-          </Form.Group>
+        {/* select */}
+        <Form.Group className="mb-3">
+          <Form.Select
+            className="form-select"
+            {...register("genero", {
+              required: true,
+            })}
+          >
+            <option value="">Selecciona un género</option>
+            <option value="Drama">Drama</option>
+            <option value="Comedia">Comedia</option>
+            <option value="Suspenso">Suspenso</option>
+            <option value="Terror">Terror</option>
+            <option value="Amor">Amor</option>
+            <option value="Documental">Documental</option>
+            <option value="Documental">Animación</option>
+            <option value="Documental">Acción</option>
+          </Form.Select>
+        </Form.Group>
 
-          {/* select */}
-          <Form.Group className="mb-3">
-            <Form.Select
-              className="form-select"
-              {...register("genero",
-                {
-                  required: true
-                })}>
-              <option value="">Selecciona un género</option>
-              <option value="Drama">Drama</option>
-              <option value="Comedia">Comedia</option>
-              <option value="Suspenso">Suspenso</option>
-              <option value="Terror">Terror</option>
-              <option value="Amor">Amor</option>
-              <option value="Documental">Documental</option>
-              <option value="Documental">Animación</option>
-              <option value="Documental">Acción</option>
-            </Form.Select>
-          </Form.Group>
+        {/* ruta de imagen */}
+        <Form.Group className="my-3">
+          <Form.Label className="text-light">Ingrese ruta de imagen</Form.Label>
+          <Form.Control
+            type="url"
+            placeholder="Ingrese ruta de imagen"
+            {...register("rutaImg", { required: true })}
+          />
+        </Form.Group>
 
-          {/* ruta de imagen */}
-          <Form.Group className="my-3">
-            <Form.Label className="text-light">Ingrese ruta de imagen</Form.Label>
-            <Form.Control
-              type="url"
-              placeholder="Ingrese ruta de imagen"
-              {...register("rutaImg", { required: true })}
-            />
-          </Form.Group>
-
-          {/* boton */}
-          <div className="text-center">
-            <Button variant="dark" type='submit'>
-              Crear Pelicula
-            </Button>
-          </div>
-        </Form>
-        <Peliculas peliculas={peliculas} borrarPelicula={borrarPelicula} />
-      </Container>
-    );
-  
-}
+        {/* boton */}
+        <div className="text-center">
+          <Button variant="dark" type="submit">
+            Crear Pelicula
+          </Button>
+        </div>
+      </Form>
+      <Peliculas peliculas={peliculas} borrarPelicula={borrarPelicula} />
+    </Container>
+  );
+};
 export default FormularioPelicula;
-
